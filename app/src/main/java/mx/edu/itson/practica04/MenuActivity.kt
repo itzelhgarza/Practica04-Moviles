1742a6af37671b4df2f8ab18de9be981a9fd5e20
itzelhgarza <itzel.garza216628@potros.itson.edu.mx>
2023-02-26 14:56:02 -0700

    Practica 05

--- app/src/main/java/mx/edu/itson/practica04/MenuActivity.kt
+++ app/src/main/java/mx/edu/itson/practica04/MenuActivity.kt
@@ -1,11 +1,20 @@
 package mx.edu.itson.practica04
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        val btn_Antojitos: Button = findViewById(R.id.button_antojitos)
+
+        btn_Antojitos.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/mx/edu/itson/practica04/Product.kt
@@ -0,0 +1,6 @@
+package mx.edu.itson.practica04
+
+data class Product(var name: String,
+                   var image: Int,
+                   var description: String,
+                   var price: Double)
